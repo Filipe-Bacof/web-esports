42888ba816864d35edf5433b85476fffc6d019bf
Filipe Bacof <filipebacof@gmail.com>
2022-09-18 13:10:14 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './styles.css';
+import './styles/main.css';
 
 function App() {
   return (
-    <div>
+    <div className='w-8 h-8 bg-violet-500'>
     </div>
   ) 
 }
+
+// parei em 25 minutos da aula 2
+// este conteudo é muito avançado,
+// creio que primeiro eu deva aprender react puro
+// sem utilizar tantas frameworks
 
 export default App
